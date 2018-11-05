00d78f1a0e218529b67c340657cc5e764bdde2c9
Felix Wetell <felix.wetell@gmail.com>
2018-11-05 11:25:43 +0100

    Removed "/public" from asset helper links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,9 +26,9 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/3.0.2/vendors/scrolloverflow.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.3.5/js/swiper.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
-		<script src="{{ asset('/public/js/morphext.min.js') }}"></script>
+		<script src="{{ asset( '/js/morphext.min.js' ) }}"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-		<script src="{{ asset('/public/js/main.js') }}"></script>
+		<script src="{{ asset( '/js/main.js' ) }}"></script>
 	</body>
 </html>
