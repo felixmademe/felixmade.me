38877418ead93dd642a43b0a4110bb511ede0f9f
Felix Wetell <felix.wetell@gmail.com>
2018-11-13 16:53:28 +0100

    App ready for production

--- resources/views/mail/view.blade.php
+++ resources/views/mail/view.blade.php
@@ -2,8 +2,8 @@
 	<head>
 		<title>felix made me</title>
 		<meta charset="utf-8">
-        <link rel="stylesheet" href="{{ secure_asset( '/public/css/mail.min.css' ) }}">
-		<link rel="icon" href="{{ secure_asset( '/public/img/logo.ico' ) }}">
+        <link rel="stylesheet" href="{{ secure_secure_asset( '/public/public/css/mail.min.css' ) }}">
+		<link rel="icon" href="{{ secure_secure_asset( '/public/public/img/logo.ico' ) }}">
         <style>
 		@font-face{font-family:Raleway;src:url("../font/Raleway-Medium.ttf"),url("../font/Raleway-MediumItalic.ttf"),url("../font/Raleway-Regular.ttf")}body{font-family:'Raleway', sans-serif !important;font-size:12px !important;width:100vw !important}h1{font-size:3em;margin-bottom:0px !important}p{font-size:1.2rem}a{font-size:1.2rem;color:#3E3E3E !important;text-decoration:none !important}hr{height:3px;border-radius:100%;background-color:#8C7AE6 !important}.small-text{font-size:1rem}.grey{color:#3E3E3E !important;text-decoration:none !important}.green{color:#7BED9F !important}.darkgreen{color:#2ED573 !important}.purple{color:#9C88FF !important}.darkpurple{color:#8C7AE6 !important}.white{background-color:#FFF;padding:20px 40px}.background{padding:20px 0 !important}.background .center{text-align:center}
         </style>
