223817001d233c6d8fd01789b448a1c6e78ad779
Felix Wetell <felix.wetell@gmail.com>
2020-05-19 15:28:36 +0200

    Updates secret view content and layout

--- resources/views/secret.blade.php
+++ resources/views/secret.blade.php
@@ -2,22 +2,17 @@
 
 @section( 'content' )
 
-<div class="container secret">
-    <div class="row">
-        <div class="col-md-8 offset-md-2 text-center">
-            <br>
-            <h2><span class="purple">you</span> found the <span class="green">secret</span> page</h2>
-            <p>
-                while I have your attention, please take a moment to share this page
-                and let others know about this website
-                <br><br>
-                here is a video of a cat playing piano:
-            </p>
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe width="" height="" src="https://www.youtube.com/embed/J---aiyznGQ"
-                        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen class="embed-responsive-item"></iframe>
-            </div>
+<div class="h-100 d-flex align-items-center justify-content-center">
+    <div class="text-center">
+        <br>
+        <h2><span class="purple">you</span> found the <span class="green">secret</span> page</h2>
+        <p>
+            here is a video of a cat playing piano
+        </p>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe width="" height="" src="https://www.youtube.com/embed/J---aiyznGQ"
+                    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen class="embed-responsive-item"></iframe>
         </div>
     </div>
 </div>
