8256dd2a49e4e43e6820182540d0ce57b825660c
Felix Wetell <felix.wetell@gmail.com>
2020-12-14 02:33:50 +0100

    Removes swiper and updates npm

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -37,7 +37,7 @@ class ContactController extends Controller
         $context = stream_context_create( $options );
         $result = file_get_contents( $url, false, $context );
         $json = json_decode( $result );
-        
+
         if( $json->success != true )
         {
             return response()->json( [ 'result' => 'reCAPTCHA error' ], 200);
