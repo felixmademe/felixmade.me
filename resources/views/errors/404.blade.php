e2376f81fe4c22a6b4772b324299eafea50d87df
Felix Wetell <felix.wetell@gmail.com>
2021-01-28 00:10:03 +0100

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Not Found'))
+@section('title', __('Not asdad'))
 @section('code', '404')
-@section('message', __('Not Found'))
+@section('message', __('Not asdad'))
