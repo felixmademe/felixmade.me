4e4b2f082cfed0d7d07477d94d8b8efd4fedb5ee
Felix Wetell <felix.wetell@gmail.com>
2021-03-10 19:16:38 +0100

    Adds hover effect on mouseover and -leave

--- resources/js/components/board.js
+++ resources/js/components/board.js
@@ -1,26 +1,50 @@
 if (window.location.href.indexOf('board') > -1) {
     let messages = $('.message');
     let colours = ['#05C46B', '#F53B57', '#1E272E', '#FFF'];
-    let message, colour;
 
     for(let i = 0; i < messages.length; i++) {
-         message = messages[i];
+        let message, colour, colourText, boxShadowColour, colourBorder, mes;
+        message = messages[i];
 
-         // Gets random colour on message containers
-         colour = colours[Math.floor(Math.random() * colours.length)];
+        // Gets random colour on message containers
+        colour = colours[Math.floor(Math.random() * colours.length)];
+        colourText = '#FFF'
+        boxShadowColour = '#1E272E';
 
-        message.style.backgroundColor = colour;
-        message.style.color = '#FFF';
-        message.style.boxShadow = '10px 10px #1E272E';
+        switch (colour) {
+            case '#FFF':
+                colourText = '#1E272E';
+                colourBorder = colourText;
 
-         switch (colour) {
-             case '#FFF':
-                message.style.color = '#1E272E';
-                message.style.border = 'solid 2px #1E272E';
+                message.style.border = 'solid 2px ' + colourBorder;
                 break;
 
-             case '#1E272E':
-                 message.style.boxShadow = '10px 10px #F53B57';
-         }
+            case '#1E272E':
+                boxShadowColour = '#F53B57';
+                break;
+
+        }
+
+        message.style.backgroundColor = colour;
+        message.style.color = colourText;
+        message.style.boxShadow = '10px 10px ' + boxShadowColour;
+
+        message.addEventListener('mouseover', function(event) {
+            if(event.currentTarget === event.target)
+            {
+                mes = event.target
+                mes.style.boxShadow = '-10px -10px ' + boxShadowColour;
+            }
+
+        }, false);
+
+        message.addEventListener('mouseleave', function(event) {
+            if(event.currentTarget === event.target)
+            {
+                mes = event.target
+                mes.style.boxShadow = '10px 10px ' + boxShadowColour;
+            }
+
+        }, false);
     }
 }
