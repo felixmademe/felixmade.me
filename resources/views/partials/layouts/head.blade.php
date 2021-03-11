968c6144f76489e855090beb8c69a02d73043b2a
Felix Wetell <felix.wetell@gmail.com>
2021-03-11 20:51:35 +0100

    Adds service worker and PWA functionality #31

--- resources/views/partials/layouts/head.blade.php
+++ resources/views/partials/layouts/head.blade.php
@@ -43,7 +43,6 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset( 'img/favi/apple-touch-icon.png' ) }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset( 'img/favi/favicon-32x32.png' ) }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset( 'img/favi/favicon-16x16.png' ) }}">
-<link rel="manifest" href="{{ asset( 'img/favi/site.webmanifest' ) }}">
 <link rel="manifest" href="{{ asset('/manifest.json') }}">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
@@ -62,7 +61,6 @@
 @endif
 
 <script src='https://www.google.com/recaptcha/api.js?render={{ config( 'recaptcha.key.site' ) }}&render=explicit'></script>
-
 <script defer>
     grecaptcha.ready(function() {
         grecaptcha.execute( '{{ config( 'recaptcha.key.site' ) }}', { action: 'contactForm' } ).then( function( token )
