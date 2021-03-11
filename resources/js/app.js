61d51ed4921765addc39b7b23f6da962c1dd0245
Felix Wetell <felix.wetell@gmail.com>
2021-03-11 21:53:45 +0100

    Refactors code placement

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
     require('ga-lite');
     require('cookieconsent');
 
-    require('./components/analytics');
-    require('./components/consent');
-    require('./components/sw');
+    require('./modules/analytics');
+    require('./modules/consent');
+    require('./modules/sw');
+
     require('./components/contact');
     require('./components/dragscroll');
     require('./components/board');
+
 });
