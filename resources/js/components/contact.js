d0a77ad023a441ef208338c7bdea7e449c41de14
Felix Wetell <felix.wetell@gmail.com>
2021-03-16 03:06:31 +0100

    Updates contact js file

--- resources/js/components/contact.js
+++ resources/js/components/contact.js
@@ -1,49 +1,48 @@
 $( "#alert" ).hide();
-    $("#contactForm").on("submit", function (e) {
-        $("#alert").hide();
-        $("#alert p").text("");
+$("#contactForm").on("submit", function (e) {
+    $("#alert").hide();
+    $("#alert p").text("");
 
-        e.preventDefault();
-        e.stopPropagation();
+    e.preventDefault();
+    e.stopPropagation();
 
-        let name = $("input[type=text]").val();
-        let email = $("input[type=email]").val();
-        let text = $("textarea").val();
-        let checkbox = $("input[type=checkbox]").is(":checked");
-        let policy = false;
-        let recaptcha = $("#recaptcha").val();
+    let name = $("input[type=text]").val();
+    let email = $("input[type=email]").val();
+    let text = $("textarea").val();
+    let checkbox = $("input[type=checkbox]").is(":checked");
+    let policy = false;
+    let recaptcha = $("#recaptcha").val();
 
-        if (checkbox) {
-            policy = true;
+    if (checkbox) {
+        policy = true;
+    }
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    });
+    let ajax = $.ajax(
+        {
+            type: 'POST',
+            url: "/contactForm",
+            data:
+                {
+                    name: name,
+                    email: email,
+                    text: text,
+                    policy: policy,
+                    recaptcha: recaptcha,
+                },
+            dataType: 'json',
+            success: function (data) {
+                $("#alert p").text(data.result);
+                $("#contactForm")[0].reset();
+                $("#alert").fadeIn(400).delay(10000).fadeOut(400);
+            },
+            error: function (data) {
+                $("#alert p").text(data.error);
+                $("#alert").fadeIn(400).delay(10000).fadeOut(400);
             }
         });
-        let ajax = $.ajax(
-            {
-                type: 'POST',
-                url: "/contactForm",
-                data:
-                    {
-                        name: name,
-                        email: email,
-                        text: text,
-                        policy: policy,
-                        recaptcha: recaptcha,
-                    },
-                dataType: 'json',
-                success: function (data) {
-                    $("#alert p").text(data.result);
-                    $("#contactForm")[0].reset();
-                    $("#alert").fadeIn(400);
-                    $("#alert").delay(10000).fadeOut(400);
-                },
-                error: function (data) {
-                    $("#alert p").text(data.error);
-                    $("#alert").fadeIn(400);
-                }
-            });
-    });
+});
