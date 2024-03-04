85d91a359bd2cd91517a8a6e0554cd21036a40a8
Felix Wetell <felix.wetell@gmail.com>
2024-03-04 12:31:38 +0100

    Adds routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,50 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+})->name('start');
+
+Route::get('/policy', function () {
+    return view('policy');
+})->name('policy');
+
+Route::get('/support', function () {
+    return view('support');
+})->name('support');
+
+Route::get('/vacation', function () {
+    return view('vacation');
+})->name('vacation');
+
+Route::post( '/contactForm', 'ContactController@contactForm' );
+
+Route::get('/secret', function () {
+    $name = 'Felix Wetell';
+    $email = config('mail.from.address');
+    $text = "please save me";
+
+    return new App\Mail\Contact($name, $email, $text);
+})->name('secret');
+
+
+Route::get('/board', 'ShowNotes')->name('board');
+if(config('app.env') === 'local') {
+    Route::post('/board/post', 'CreateNote')->name('board.post')
+    ->middleware('throttle:1,5');
+} else {
+    Route::post('/board/post', 'CreateNote')->name('board.post')
+        ->middleware('throttle:1,60');
+}
+
+
+Route::prefix('games')->group(function () {
+    Route::get('/', 'ShowGames')->name('games');
+
+    Route::get('/recnuob', function () {
+        return view('games.recnuob');
+    })->name('games.recnuob');
+
+    Route::get('/tutorial', function () {
+        return view('games.tutorial');
+    })->name('games.tutorial');
 });
