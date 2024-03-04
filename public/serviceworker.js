dad86eee43adedadc046c763a78a2039a5916086
Felix Wetell <felix.wetell@gmail.com>
2024-03-04 12:29:30 +0100

    Adds public folder

---
+++ public/serviceworker.js
@@ -0,0 +1,56 @@
+var staticCacheName = "pwa-v" + new Date().getTime();
+var filesToCache = [
+    '/',
+    '/css/app.css',
+    '/js/app.js',
+    '/img/favi/android-chrome-192x192.png',
+    '/img/favi/android-chrome-512x512.png',
+    '/img/favi/apple-touch-icon.png',
+    '/img/favi/favicon-16x16.png',
+    '/img/favi/favicon-32x32.png',
+    '/img/favi/maskable_icon_x48.png',
+    '/img/favi/maskable_icon_x72.png',
+    '/img/favi/maskable_icon_x96.png',
+    '/img/favi/maskable_icon_x128.png',
+    '/img/favi/maskable_icon_x192.png',
+    '/img/favi/maskable_icon_x384.png',
+    '/img/favi/maskable_icon_x512.png',
+];
+
+// Cache on install
+self.addEventListener("install", event => {
+    this.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    )
+});
+
+// Clear cache on activate
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith("pwa-")))
+                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+// Serve from Cache
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('offline');
+            })
+    )
+});
