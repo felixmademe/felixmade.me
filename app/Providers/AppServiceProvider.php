265ce9951584e82ee9f1637906f0a5b174b628a9
Felix Wetell <felix.wetell@gmail.com>
2024-03-04 12:26:05 +0100

    Adjusts providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength( 191 );
+
+        if (env('APP_ENV') == 'production')
+        {
+            URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
